3a7a6afb7ea6d690988d5e3b142836001be293e3
iurygdeoliveira <iury.oliveira@ifto.edu.br>
2017-11-28 22:03:32 -0300

    Organização de Pastas, Programação inicial do MVC, programação da view home, ajustes no AplicacaoMVC

--- classes/aplicacao/AplicacaoMVC.class.php
+++ classes/aplicacao/AplicacaoMVC.class.php
@@ -54,60 +54,11 @@ class AplicacaoMVC {
             // FIM :)
             return;
         endif;
-//
-//        // Se o arquivo do controlador não existir, exibe pagina não encontrada
-//        if (!file_exists(ABSPATH . '/controllers/' . $this->controlador . '.php')) {
-//            // Página não encontrada
-//            require_once ABSPATH . $this->not_found;
-//
-//            // FIM :)
-//            return;
-//        }
-//
-//        // Inclui o arquivo do controlador
-//        require_once ABSPATH . '/controllers/' . $this->controlador . '.php';
-//
-//        // Remove caracteres inválidos do nome do controlador para gerar o nome
-//        // da classe. Se o arquivo chamar "news-controller.php", a classe deverá
-//        // se chamar NewsController.
-//        $this->controlador = preg_replace('/[^a-zA-Z]/i', '', $this->controlador);
-//
-//        // Se a classe do controlador indicado não existir, não faremos nada
-//        if (!class_exists($this->controlador)) {
-//            // Página não encontrada
-//            require_once ABSPATH . $this->not_found;
-//
-//            // FIM :)
-//            return;
-//        } // class_exists
-//        // Cria o objeto da classe do controlador e envia os parâmentros
-//        $this->controlador = new $this->controlador($this->parametros);
-//
-//        // Remove caracteres inválidos do nome da ação (método)
-//        $this->acao = preg_replace('/[^a-zA-Z]/i', '', $this->acao);
-//
-//        // Se o método indicado existir, executa o método e envia os parâmetros
-//        if (method_exists($this->controlador, $this->acao)) {
-//            $this->controlador->{$this->acao}($this->parametros);
-//
-//            // FIM :)
-//            return;
-//        } // method_exists
-//        // Sem ação, chamamos o método index
-//        if (!$this->acao && method_exists($this->controlador, 'index')) {
-//            $this->controlador->index($this->parametros);
-//
-//            // FIM :)
-//            return;
-//        } // ! $this->acao 
-//        // Página não encontrada
-//        require_once ABSPATH . $this->not_found;
-//
-//        // FIM :)
-//        return;
+
+        // Fim do __construct
     }
 
-// Fim do __construct
+
 
     /**
      * Obtém parâmetros de $_GET['path']
